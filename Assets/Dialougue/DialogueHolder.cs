6a18d6de1e3b4f103447a57960b02de7420f04b2
Tyler Molander <tylermolander@gmail.com>
2018-08-31 18:50:45 -0400

    Dialogue

---
+++ Assets/Dialougue/DialogueHolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour
+{
+
+    public string dialogue;
+    DialogueManager dMAn;
+
+    public string[] dialogueLines;
+
+	void Start ()
+	{
+	    dMAn = FindObjectOfType<DialogueManager>();
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                //dMAn.ShowBox(dialogue);
+
+                if (!dMAn.dialogActive)
+                {
+                    dMAn.dialogLines = dialogueLines;
+                    dMAn.currentLine = 0;
+                    dMAn.ShowDialogue();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Dialougue/DialogueManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour 
+{
+
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogActive;
+
+    public string[] dialogLines;
+    public int currentLine;
+
+    void Start()
+    {
+
+    }
+
+	void Update () 
+	{
+	    if (dialogActive && Input.GetKeyDown(KeyCode.L))
+	    {
+            //dBox.SetActive(false);
+	        //dialogActive = false;
+
+	        currentLine++;
+	    }
+
+	    if (currentLine >= dialogLines.Length)
+	    {
+	        dBox.SetActive(false);
+	        dialogActive = false;
+            currentLine = 0;
+	    }
+
+	    dText.text = dialogLines[currentLine];
+	}
+
+    public void ShowBox(string dialogue)
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+    public void ShowDialogue()
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+    }
+}
