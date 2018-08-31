657433b26b3c617fe0e2df38bc7ec6c4be4e805e
Tyler Molander <tylermolander@gmail.com>
2018-08-31 01:11:27 -0400

    FinallyGoodWalk

--- Assets/NewMovement.cs
+++ Assets/NewMovement.cs
@@ -6,23 +6,30 @@ public class NewMovement : MonoBehaviour
 {
     public float speed;
     private Animator animator;
+    bool playerMoving;
+    Vector2 lastMove;
 
 	void Start ()
 	{
 	    animator = GetComponent<Animator>();
 	}
 	
-	void Update () 
+	void Update ()
 	{
+	    playerMoving = false;
 
 	    if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f ||
 	        Input.GetAxisRaw("Vertical") > 0.5f   || Input.GetAxisRaw("Vertical") < -0.5f)
 	    {
-	       transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 
+	        transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 
 	                                        Input.GetAxisRaw("Vertical")   * speed * Time.deltaTime, 0f));
-            animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-	        animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            playerMoving = true;
 	    }
+        animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+	    animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+	    animator.SetBool("PlayerMoving", playerMoving);
+	    animator.SetFloat("LastMoveX", lastMove.x);
+	    animator.SetFloat("LastMoveY", lastMove.y);
 	}
 }
