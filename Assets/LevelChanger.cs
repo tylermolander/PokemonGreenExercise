ad29d6c8c3acb6feb757a1b431a83f39fb4f379a
Tyler Molander <tylermolander@gmail.com>
2018-09-01 18:34:28 -0400

    FinishFadeOut

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    int levelToLoad;
+	
+	void Update () 
+	{
+		
+	}
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
