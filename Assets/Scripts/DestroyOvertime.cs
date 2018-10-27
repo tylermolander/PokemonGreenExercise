44e8d316f16b95a562fd7c31c025d2e19c4fce16
tylermolander <tylermolander@gmail.com>
2018-10-26 23:35:37 -0400

    DamageFloatingNumbers

---
+++ Assets/Scripts/DestroyOvertime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOvertime : MonoBehaviour
+{
+
+    public float timeToDestroy;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update ()
+	{
+	    timeToDestroy -= Time.deltaTime;
+
+	    if (timeToDestroy <= 0)
+	    {
+            Destroy(gameObject);
+	    }
+	}
+}
--- Assets/Scripts/HurtEnemy.cs
+++ Assets/Scripts/HurtEnemy.cs
@@ -8,14 +8,17 @@ public class HurtEnemy : MonoBehaviour
     public int damageToGive;
     public GameObject damageBurst;
     public Transform hitPoint;
+    public GameObject damageNumber; //damage numbers prefab, which contains UI text object
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Enemy")
         {
-            //Destroy(other.gameObject);
             other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
             Instantiate(damageBurst, hitPoint.position, hitPoint.rotation);
+            var clone = (GameObject) Instantiate(damageNumber, hitPoint.position, Quaternion.Euler(Vector3.zero)); //(gameobject) "casts" the instance into a clone?
+            clone.GetComponent<FloatingNumbers>().damageNumber = damageToGive; //the damage number (a text UI) of the instance is assigned the damage pt of the weapon
+
         }
     }
 
