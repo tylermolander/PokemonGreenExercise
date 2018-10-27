54c3a659122d1c25c7170e9101c2cb958aaaa2b3
tylermolander <tylermolander@gmail.com>
2018-10-27 14:55:25 -0400

    QuestManager

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QuestManager : MonoBehaviour
+{
+    
+    public QuestObject[] quests;
+    public bool[] questCompleted;
+
+    public DialogueManager theDialogueManager;
+
+
+	void Start () 
+	{
+		questCompleted = new bool[quests.Length];
+
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+    public void ShowQuestText(string questText)
+    {
+        theDialogueManager.dialogLines = new string[1];
+        theDialogueManager.dialogLines[0] = questText;
+
+        theDialogueManager.currentLine = 0;
+        theDialogueManager.ShowDialogue();
+
+    }
+
+}
---
+++ Assets/Scripts/QuestObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour 
+{
+
+    //think about what individual quests need to do;
+    //set up, engaged (gameobject quest is active, unable to start again, reward...
+
+    public int questNumber;
+    public QuestManager theQuestManager;
+
+    public string startText;
+    public string endText;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+    public void StartQuest()
+    {
+        theQuestManager.ShowQuestText(startText);
+    }
+
+    public void EndQuest()
+    {
+        theQuestManager.ShowQuestText(endText);
+        theQuestManager.questCompleted[questNumber] = true;  
+        gameObject.SetActive(false);
+
+    }
+}
